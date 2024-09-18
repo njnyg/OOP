06b12420fd4074ed2cea1de843218a309b57159b
Nina <nina.nygren99@gmail.com>
2024-09-18 17:51:14 +0300

    Finished exercise 5

--- Harjoitus 2/Luokkahuone/src/Luokkahuone.java
+++ Harjoitus 2/Luokkahuone/src/Luokkahuone.java
@@ -2,9 +2,27 @@ public class Luokkahuone {
 
     private String luokanKoodi;
     private int istumapaikat;
+    private int varatutPaikat;
 
-    Luokkahuone(String luokanKoodi, int istumapaikat) {
+    Luokkahuone(String luokanKoodi, int istumapaikat, int varatutPaikat) {
         this.luokanKoodi = luokanKoodi;
         this.istumapaikat = istumapaikat;
+        this.varatutPaikat = varatutPaikat;
+    }
+
+    public void tulostaLuokkahuone() {
+        System.out.println("Luokan koodi: " + luokanKoodi);
+        System.out.println("Istumapaikat: " + istumapaikat);
+        System.out.println("Varatut paikat: " + varatutPaikat);
+    }
+
+    public void varaaPaikka() {
+        this.varatutPaikat++;
+        System.out.println("Varataan paikkaa...");
+    }
+
+    public void tulostaTilanne() {
+        int vapaatPaikat = istumapaikat - varatutPaikat;
+        System.out.println("Vapaita paikkoja jäljellä: " + vapaatPaikat);
     }
 }
---
+++ Harjoitus 2/Luokkahuone/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Luokkahuone huone1 = new Luokkahuone("C2018", 16, 1);
+        huone1.tulostaLuokkahuone();
+        huone1.varaaPaikka();
+        huone1.tulostaTilanne();
+    }
+}
