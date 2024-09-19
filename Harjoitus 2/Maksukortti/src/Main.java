b4db171d3e9d7a1bec48ca426d780500e5417ff4
Nina <nina.nygren99@gmail.com>
2024-09-20 00:40:20 +0300

    Finished exercise 10

--- Harjoitus 2/Maksukortti/src/Main.java
+++ Harjoitus 2/Maksukortti/src/Main.java
@@ -1,2 +1,41 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        Maksukortti liisanKortti = new Maksukortti("Liisa", 20);
+        Maksukortti nuutinKortti = new Maksukortti("Nuutti", 50);
+
+        liisanKortti.syoMaukkaasti();
+        nuutinKortti.syoEdullisesti();
+
+        System.out.println(liisanKortti);
+        System.out.println(nuutinKortti);
+
+        liisanKortti.lataaRahaa(10);
+        nuutinKortti.syoMaukkaasti();
+
+        System.out.println(liisanKortti);
+        System.out.println(nuutinKortti);
+
+        liisanKortti.syoMaukkaasti();
+        liisanKortti.syoMaukkaasti();
+        nuutinKortti.lataaRahaa(50);
+
+        System.out.println(liisanKortti);
+        System.out.println(nuutinKortti);
+
+        liisanKortti.syoMaukkaasti();
+        liisanKortti.syoEdullisesti();
+        nuutinKortti.syoEdullisesti();
+
+        System.out.println(liisanKortti);
+        System.out.println(nuutinKortti);
+
+        nuutinKortti.lataaRahaa(-50);
+        liisanKortti.lataaRahaa(10);
+
+        System.out.println(liisanKortti);
+        System.out.println(nuutinKortti);
+
+    }
 }
--- Harjoitus 2/Maksukortti/src/Maksukortti.java
+++ Harjoitus 2/Maksukortti/src/Maksukortti.java
@@ -1,2 +1,39 @@
 public class Maksukortti {
+
+    private String omistaja;
+    private double saldo;
+    private double edullinenLounas = 2.95;
+    private double maukasLounas = 6.95;
+
+    Maksukortti(String omistaja, double saldo) {
+        this.omistaja = omistaja;
+        this.saldo = saldo;
+    }
+
+    public String toString(){
+        return "Kortin omistaja: " + omistaja + ", kortilla on saldoa: " + saldo + " euroa.";
+    }
+
+    public void syoEdullisesti() {
+        if (saldo < edullinenLounas) {
+            System.out.println("Edullista lounasta ei voi myydä, kortilla ei ole riittävästi saldoa");
+        } else {
+            saldo -= edullinenLounas;
+        }
+    }
+
+    public void syoMaukkaasti() {
+        if (saldo < maukasLounas) {
+            System.out.println("Maukasta lounasta ei voi myydä, kortilla ei ole riittävästi saldoa.");
+        } else {
+            saldo -= maukasLounas;
+        }
+    }
+
+    public void lataaRahaa(int ladattavaSumma) {
+        if (ladattavaSumma > 0) {
+            saldo += ladattavaSumma;
+        }
+    }
 }
+
